70b342d74b70dbc4e0920a15ecf602bc70e0f573
Eduardo <eduaraujp760@gmail.com>
2023-07-05 12:05:25 -0400

    Exercicios 1,2  e 3 resolvidos

---
+++ script.js
@@ -0,0 +1,8 @@
+const titlee = document.querySelector("h1");
+console.log(titlee);
+
+const titleee = document.getElementById("title");
+console.log(titleee);
+
+const titulo = document.getElementsByClassName("title");
+console.log(titulo[0]);
